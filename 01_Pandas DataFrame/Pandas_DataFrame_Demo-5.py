40ca9df50db89f0ecea483dfff9c025499421bc5
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-14 18:38:11 +0530

    Update Pandas_DataFrame_Demo-5.py

--- 01_Pandas DataFrame/Pandas_DataFrame_Demo-5.py
+++ 01_Pandas DataFrame/Pandas_DataFrame_Demo-5.py
@@ -6,7 +6,6 @@ data = {
     'Rank': [1, 2, 3, 4, 5, 6, 7],
     'Marks': [99, 71, 89, 69, 50, 33, 83]
 }
-
 # Index Arguments to set your indexes
 df = pd.DataFrame(data, index = ['Student1', 'Student2', 'Student3', 'Student4', 'Student5', 'Student6', 'Student7'])
 
