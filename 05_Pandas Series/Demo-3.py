3aac0b1ea9cd1ce31bbd37f2d54d5c0e3b149c08
Dharm Patel <dharmpatel311287@gmail.com>
2026-08-14 18:38:55 +0530

    Improve readability by adding a blank line
    Added a blank line for better readability.

--- 05_Pandas Series/Demo-3.py
+++ 05_Pandas Series/Demo-3.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # Data to be stored in the Pandas Series
 data = [10, 20, 40, 80, 100]
+
 # Create a Series using the Series() method
 s = pd.Series(data, index=["RowA", "RowB", "RowC", "RowD", "RowE"])
 
